06f4cc7929bf346d4d685fdc17711ccc15da699f
RealTux678 <85577988+RealTux678@users.noreply.github.com>
2022-07-20 17:26:08 +0200

    lib name not hard coded

--- java/Generateur_ANFR.sh
+++ java/Generateur_ANFR.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 script_path="$(realpath "$0")"
 DIR_script="$(dirname "${script_path}")"
-java -cp "${DIR_script}/lib/sqlite-jdbc-3.36.0.1.jar:${DIR_script}/" Main $1
+java -cp "${DIR_script}/lib/*:${DIR_script}/" Main $1
